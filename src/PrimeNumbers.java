466fa2cbe54b97cee20c7320ad6ea1d9aa39ce48
playrey2010 <playrey2010@gmail.com>
2019-09-06 11:40:31 -0400

    added 0 and 1, fixed for loop

--- src/PrimeNumbers.java
+++ src/PrimeNumbers.java
@@ -9,17 +9,22 @@ public class PrimeNumbers {
 
         System.out.print("Please enter a number and I'll let you know if it is prime or not: ");
         num1 = sc.nextInt();
-        for (int i = 2; i < num1; i++) {
-            if (num1%i == 0) {
-                isPrime = false;
-                break;
-            } else {isPrime = true;}
+        if (num1 == 0 || num1 == 1){
+            System.out.println(num1 + " is not a prime number.");
+        } else {
+            for (int i = 2; i < num1; i++) {
+                if (num1%i == 0) {
+                    isPrime = false;
+                    break;
+                } else {isPrime = true;}
+            }
+            if (isPrime) {
+                System.out.println(num1 + " is a prime number.");
+            } else {
+                System.out.println(num1 + " is not a prime number.");
+            }
         }
 
-        if (isPrime) {
-            System.out.println(num1 + " is a prime number.");
-        } else {
-            System.out.println(num1 + " is not a prime number.");
-        }
+
     }
 }
